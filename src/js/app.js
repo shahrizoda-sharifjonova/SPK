25de000fe14086b4b7ee32ec607e346d94d9af6a
shahrizoda <shahrizodadeveloping@gmail.com>
2022-08-29 22:42:36 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,34 @@ import * as functions from "./modules/functions.js";
 
 functions.isWebp();
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-// const swiper = new Swiper();
+const swiper = new Swiper();
+
+var mySwiper = new Swiper('.photos__swiper', {
+    modules: [Navigation],
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3000,
+    },
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 80,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+})
 
 const menu = document.querySelector('.menu');
 const list = document.querySelector('.header__list');
